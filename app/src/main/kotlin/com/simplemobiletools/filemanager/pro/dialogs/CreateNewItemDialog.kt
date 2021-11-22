ef4979bfb2ad01b2b9a3e055d3d523aeae67dd9a
kingyaaa <191220004@smail.nju.edu.cn>
2021-11-23 00:09:22 +0800

    add path_select

--- app/src/main/kotlin/com/simplemobiletools/filemanager/pro/dialogs/CreateNewItemDialog.kt
+++ app/src/main/kotlin/com/simplemobiletools/filemanager/pro/dialogs/CreateNewItemDialog.kt
@@ -2,64 +2,88 @@ package com.simplemobiletools.filemanager.pro.dialogs
 
 import android.view.View
 import androidx.appcompat.app.AlertDialog
+import com.simplemobiletools.commons.dialogs.FilePickerDialog
 import com.simplemobiletools.commons.extensions.*
 import com.simplemobiletools.filemanager.pro.R
 import com.simplemobiletools.filemanager.pro.activities.SimpleActivity
 import com.simplemobiletools.filemanager.pro.helpers.RootHelpers
+import com.simplemobiletools.filemanager.pro.helpers.Config
 import kotlinx.android.synthetic.main.dialog_create_new.view.*
+import kotlinx.android.synthetic.main.dialog_save_as.view.*
 import java.io.File
 import java.io.IOException
 
-class CreateNewItemDialog(val activity: SimpleActivity, val path: String, val callback: (success: Boolean) -> Unit) {
+import com.simplemobiletools.commons.activities.BaseSimpleActivity
+import com.simplemobiletools.commons.dialogs.ConfirmationDialog
+import com.simplemobiletools.commons.extensions.*
+
+class CreateNewItemDialog(val activity: BaseSimpleActivity, var path: String, val callback: (success: Boolean) -> Unit) {
     private val view = activity.layoutInflater.inflate(R.layout.dialog_create_new, null)
 
     init {
-        AlertDialog.Builder(activity)
-                .setPositiveButton(R.string.ok, null)
-                .setNegativeButton(R.string.cancel, null)
+        //if (path.isEmpty()) {
+        //    path = "${activity.internalStoragePath}/${activity.getCurrentFormattedDateTime()}.txt"
+        //}
 
-                .create().apply {
-                    activity.setupDialogStuff(view, this, R.string.create_new) {
-                        //keyboard
-                        showKeyboard(view.item_name)
+        var realPath = path.toString()
+        val view = activity.layoutInflater.inflate(R.layout.dialog_create_new, null).apply {
+            create_as_path.text = activity.humanizePath(realPath)
 
-                        getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(View.OnClickListener {
-                            val name = view.item_name.value
-                            if (name.isEmpty()) {
-                                activity.toast(R.string.empty_name)
-                            } else if (name.isAValidFilename()) {
-                                val newPath = "$path/$name"
-                                if (activity.getDoesFilePathExist(newPath)) {
-                                    activity.toast(R.string.name_taken)
-                                    return@OnClickListener
-                                }
-
-                                if (view.dialog_radio_group.checkedRadioButtonId == R.id.dialog_radio_directory) {
-                                    createDirectory(newPath, this) {
-                                        callback(it)
-                                    }
-                                }
-
-                                else if(view.dialog_radio_group.checkedRadioButtonId == R.id.dialog_radio_file){
-                                    createFile(newPath, this) {
-                                        callback(it)
-                                    }
-                                }
-                                //modify:add dialog_radio_zip
-                                //zip could not be opened
-                                else{
-                                    val newPath = "$path/$name.zip"
-                                    createFile(newPath, this) {
-                                        callback(it)
-                                    }
-
-                                }
-                            } else {
-                                activity.toast(R.string.invalid_name)
-                            }
-                        })
-                    }
+            create_as_path.setOnClickListener {
+                FilePickerDialog(activity, realPath, false, false, true, true, showFavoritesButton = true) {
+                    create_as_path.text = activity.humanizePath(it)
+                    realPath = it
                 }
+            }
+        }
+
+        AlertDialog.Builder(activity)
+            .setPositiveButton(R.string.ok, null)
+            .setNegativeButton(R.string.cancel, null)
+            //.setPositiveButton("Clean",null)
+
+            .create().apply {
+                activity.setupDialogStuff(view, this, R.string.create_new) {
+                    //keyboard
+                    showKeyboard(view.item_name)
+
+                    getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(View.OnClickListener {
+                        val name = view.item_name.value
+                        if (name.isEmpty()) {
+                            //activity.toast(R.string.empty_name)
+                            //openPath(config.homeFolder)
+                        } else if (name.isAValidFilename()) {
+                            val newPath = "$realPath/$name"
+                            if (activity.getDoesFilePathExist(newPath)) {
+                                activity.toast(R.string.name_taken)
+                                return@OnClickListener
+                            }
+
+                            if (view.dialog_radio_group.checkedRadioButtonId == R.id.dialog_radio_directory) {
+                                createDirectory(newPath, this) {
+                                    callback(it)
+                                }
+                            }
+                            //modify:add dialog_radio_zip
+                            else if(view.dialog_radio_group.checkedRadioButtonId == R.id.dialog_radio_file){
+                                createFile(newPath, this) {
+                                    //config.addFavorite(fragment.currentPath)
+                                    //callback(it)
+                                }
+                            }
+                            else{
+                                val newPath = "$newPath/$name.zip"
+                                createFile(newPath, this) {
+                                    callback(it)
+                                }
+
+                            }
+                        } else {
+                            activity.toast(R.string.invalid_name)
+                        }
+                    })
+                }
+            }
     }
 
     private fun createDirectory(path: String, alertDialog: AlertDialog, callback: (Boolean) -> Unit) {
